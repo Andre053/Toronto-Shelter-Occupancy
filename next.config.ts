1f149cf3c39c35ec78f0ba3edd25b0995e2eb925
Andre053 <andre.61122@proton.me>
2025-10-28 10:32:22 -0400

    Next config and package json changes for GitHub pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // creating a static export of Next.js app
 };
 
 export default nextConfig;

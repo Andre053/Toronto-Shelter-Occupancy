b0ce32f650161d5b166adb65f89515271fa37f51
Andre053 <andre.61122@proton.me>
2025-11-04 09:54:05 -0500

    Fixed navbar link to GitHub code repository

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,7 +13,7 @@ const LinkItem = ({children, href}: {children: React.ReactNode; href: string; })
 
 export const NavBar = () => (
     <nav className="z-5 sticky top-0 w-full flex items-center justify-between bg-blue-500 p-2 mb-2 text-2xl text-white font-semibold border-b-2 border-indigo-400">
-        <Link href="https://github.com/Andre053/Toronto-Daily-Shelter-Overnight-Service-Usage" className="ml-5">
+        <Link href="https://github.com/Andre053/Toronto-Shelter-Occupancy" className="ml-5">
             <FaGithub/>
         </Link>
         <ul className="flex items-center gap-8 mt-2 mb-2 mr-5">
